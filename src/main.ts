c026f11255b614a1c90a8f64403c064d803f8a0f
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2025-09-04 11:04:47 +0530

    fix: update domain everywhere

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ function noSearchDefaultPageRender() {
           <input 
             type="text" 
             class="url-input"
-            value="https://unduck.link?q=%s"
+            value="https://unduck-sw.vercel.app?q=%s"
             readonly 
           />
           <button class="copy-button">
