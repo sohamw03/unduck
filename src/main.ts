8fa6d71ed0abf72c755b3d765225062e78b6a294
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2025-09-04 11:12:16 +0530

    fix: update desc

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ function noSearchDefaultPageRender() {
     <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
       <div class="content-container">
         <h1>Und*ck</h1>
-        <p>DuckDuckGo's bang redirects are too slow. Add the following URL as a custom search engine to your browser. Enables <a href="https://duckduckgo.com/bang.html" target="_blank">all of DuckDuckGo's bangs.</a></p>
+        <p>DuckDuckGo's bang redirects are too slow. Add the following URL as a custom search engine to your browser. Enables <a href="https://duckduckgo.com/bang.html" target="_blank">all of DuckDuckGo's bangs.</a> Now with Google AI Mode (!ai), Perplexity (!p) and ChatGPT (!gpt)</p>
         <div class="url-container"> 
           <input 
             type="text" 
